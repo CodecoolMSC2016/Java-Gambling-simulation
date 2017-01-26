1ae0ef4fc4cfd000dec3b5de8c551899666b4ebf
FerencOnodi <onodiferenc91@gmail.com>
2017-01-26 14:11:03 +0100

    Implemented Logger.java

--- Logger.java
+++ Logger.java
@@ -1,6 +1,8 @@
-import java.util.Date;
+import java.util.*;
 public class Logger {
 
 	public void log(String type, String message) {
+		Date exactTime = Calendar.getInstance().getTime();
+		System.out.println(exactTime + type + message);
 	}
 }
